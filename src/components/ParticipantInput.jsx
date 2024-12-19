74b765df4e1c1a0d213e118e215e6f1bfdd0a79a
Jocelyn-vnt <jocelyn.vinatie@etu.unilim.fr>
2024-12-19 10:58:19 +0100

    Menu done

--- src/components/ParticipantInput.jsx
+++ src/components/ParticipantInput.jsx
@@ -17,48 +17,42 @@ export function ParticipantInput({
   };
 
   return (
-    <div className="h-screen w-screen flex flex-col items-center  bg-gradient-to-b from-red-900 to-red-700 text-white py-24">
-      <div className="absolute top-0 right-4 bg-white h-20 p-5 rounded-b-full">
+    <div className="h-screen w-screen flex flex-col items-center bg-gradient-to-b from-red-900 to-red-700 text-white">
+      <div className="absolute p-4 right-4 bg-white rounded-b-full">
         <button onClick={() => setMenuOpen(!menuOpen)} className="text-black text-3xl">
           <FaBars />
         </button>
       </div>
-      {menuOpen && (
-        <div className="absolute top-0 right-0 w-2/3 h-full bg-white flex flex-col items-center justify-center">
-          <button onClick={() => setMenuOpen(false)} className="absolute top-4 right-4 text-red-800 text-3xl">
-            <FaTimes />
-          </button>
-          <div>
-            
-          </div>
-          <nav>
-            <ul className="flex flex-col items-center space-y-4">
-              <li>
-                <a href="#" className="text-red-800 font-bold text-2xl">
-                  Secret Santa
-                </a>
-              </li>
-              <li>
-                <a href="#" className="text-red-800 hover:text-gray-600">
-                  Home
-                </a>
-              </li>
-              <li>
-                <a href="#" className="text-red-800 hover:text-gray-600">
-                  About
-                </a>
-              </li>
-              <li>
-                <a href="#" className="text-red-800 hover:text-gray-600">
-                  Contact
-                </a>
-              </li>
-            </ul>
-          </nav>
+      <div className={`absolute top-0 right-0 w-2/3 h-full bg-white grid grid-rows-7 transition-transform duration-500 py-10 ${menuOpen ? 'transform translate-y-0' : 'transform -translate-y-full'}`}>
+        <div className="row-span-1 flex flex-col justify-start items-center w-full mt-8 gap-5">
+          <img src="./Members" alt="" className="w-2/3"/>
+          <p className="text-5xl text-black font-modak">John Doe</p>
         </div>
-      )}
+        <nav className="row-span-5 flex justify-center items-center">
+          <ul className="flex flex-col items-center space-y-4 text-3xl font-bold">
+            <li className="border-b border-green-800 w-full text-center pb-4">
+              <a href="#" className="text-green-800 hover:text-gray-600">
+                Home
+              </a>
+            </li>
+            <li className="border-b border-red-800 w-full text-center pb-4 ">
+              <a href="#" className="text-red-800 hover:text-gray-600">
+                About
+              </a>
+            </li>
+            <li className="w-full text-center">
+              <a href="#" className="text-black hover:text-gray-600">
+                Contact
+              </a>
+            </li>
+          </ul>
+        </nav>
+        <button onClick={() => setMenuOpen(false)} className="absolute top-4 right-4 text-red-800 text-3xl">
+          <FaTimes />
+        </button>
+      </div>
       <h1 className="text-5xl font-extrabold mb-10 text-center">
-        <div className="flex justify-center w-full">
+        <div className="flex justify-center w-full py-20">
           <img src="./Members.svg" alt="" className="w-1/2" />
         </div>
       </h1>
